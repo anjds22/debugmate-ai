259e99f33c9c5781da30d9f90571b7f2ed511fe2
anjds22 <aadarsh.jeyden@gmail.com>
2026-06-26 12:03:37 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 # ----------------------------
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
 
 # ----------------------------
 # Page Config
